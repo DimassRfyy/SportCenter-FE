4cb7b6429fd7b6afd7c328903b6d86601cf2d2cf
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2024-12-03 17:29:05 +0700

    Add routing and CheckBooking page with BottomNavbar component
    - Instal React-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import CheckBooking from "./pages/CheckBooking"
 import Home from "./pages/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 
 function App() {
   return (
     <div className="relative flex flex-col w-full min-h-screen max-w-[640px] mx-auto bg-white">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/check-booking" element={<CheckBooking />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
